6d360904f86b465dd1bddbb77427cd9f24b30a95
Gabriel Morel <gabriel@asellion.com>
2021-02-17 16:22:18 +0100

    added mock grid

--- src/components/MapGrid.vue
+++ src/components/MapGrid.vue
@@ -1,23 +1,62 @@
 <template>
-  <div>Hello from Grid</div>
+<div class="grid" :style="gridTemplateColumnStyle">
+    <div v-for="item in gridItems" :key="item.id">{{item.id}}</div>
+</div>
 </template>
 
 <script lang='ts'>
-import { defineComponent, reactive } from 'vue';
+import {
+  computed, defineComponent, reactive, toRefs,
+} from 'vue';
+
+const defaultSize = 10;
+
+interface GridItem {
+  id: number;
+}
 
 interface ComponentState {
-  gridSize: number;
+  gridItems: GridItem[];
+  gridTemplateColumnStyle: string;
 }
 
 export default defineComponent({
   name: 'MapGrid',
   setup() {
     const state: ComponentState = reactive({
-      gridSize: 100,
+      gridItems: computed(() => (new Array(defaultSize * defaultSize)
+        .fill(null)
+        .map((item: GridItem, index: number) => ({ id: (index + 1) })))),
+      gridTemplateColumnStyle: computed(() => `grid-template-columns: repeat(${defaultSize}, 1fr)`),
+
     });
+    return {
+      ...toRefs(state),
+    };
   },
 });
 </script>
 
-<style>
+<style lang="scss" scoped>
+
+.grid {
+    display: grid;
+    align-items: stretch;
+    grid-gap: 5px;
+    width: 100%;
+    height: 100%;
+
+    div::before {
+        content: "";
+        padding-bottom: 100%;
+        display: inline-block;
+        vertical-align: top;
+    }
+
+    div {
+        min-width: 30px;
+        min-height: 30px;
+        background-color: red;
+    }
+}
 </style>
