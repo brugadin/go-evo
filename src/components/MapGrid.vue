1e95dd34e1299b2d2bf2b726e7499bb165364343
Gabriel Morel <gabriel@asellion.com>
2021-02-17 15:15:41 +0100

    small clean up

---
+++ src/components/MapGrid.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>Hello from Grid</div>
+</template>
+
+<script lang='ts'>
+import { defineComponent, reactive } from 'vue';
+
+interface ComponentState {
+  gridSize: number;
+}
+
+export default defineComponent({
+  name: 'MapGrid',
+  setup() {
+    const state: ComponentState = reactive({
+      gridSize: 100,
+    });
+  },
+});
+</script>
+
+<style>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,20 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <MapGrid />
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent } from 'vue';
+import MapGrid from '@/components/MapGrid.vue'; // @ is an alias to /src
 
-@Options({
+export default defineComponent({
+  name: 'Home',
   components: {
-    HelloWorld,
+    MapGrid,
   },
-})
-export default class Home extends Vue {}
+  created() {
+    console.log('HEY THERE');
+  },
+});
 </script>
