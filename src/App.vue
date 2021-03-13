c35a040d60327204ed3426eb85aeedd98c8304ec
Gabriel Morel <gabriel.morel@gmail.com>
2021-03-13 11:51:29 +0100

    added github link

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,7 @@
   :closable="false"
   show-icon
   center>
+  <div>View source in GitHub <a class="github-repo" href="https://github.com/brugadin/go-evo">brugadin/go-evo</a></div>
 </el-alert>
   <div class="main-content">
     <div class="main-container">
@@ -29,10 +30,17 @@ export default defineComponent({
   },
 });
 </script>
-<style scoped>
+<style lang="scss" scoped>
+@import '@/element-variables.scss';
 
 .main-container, .pre-alpha-alert {
   min-width: 640px;
+}
+
+.github-repo {
+    text-decoration: none;
+    color: $--color-warning;
+    font-weight: bold;
 }
 
 @media screen and (min-width: 641px) {
